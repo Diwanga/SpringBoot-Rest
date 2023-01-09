434c8bb26511d9a949bfd6865a879eb9ac0065a5
Diwanga Amasith <diwangaamasith@gmail.com>
2023-01-09 05:37:16 +0530

    Advance Rest
     Exceptional handling, API versioning, Contend negotiation , data filtering , JPA

---
+++ SpringBootDemo/src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+
+
+import com.example.demo.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    Employee save(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    Employee getEmployeeById(String id);
+
+    String deleteEmployeeById(String id);
+}
